4c600970239cd9c6c42cce0b88d2da186b289cb4
macOSahmed <choudhurymaroof@gmail.com>
2025-01-22 12:51:28 +0530

    Create AppDelegate.swift

---
+++ ios/Runner.xcworkspace/AppDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Flutter
+import SwiftUI
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    if #available(iOS 13.0, *) {
+      let contentView = ContentView()
+      let window = UIWindow(frame: UIScreen.main.bounds)
+      window.rootViewController = UIHostingController(rootView: contentView)
+      self.window = window
+      window.makeKeyAndVisible()
+    }
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
